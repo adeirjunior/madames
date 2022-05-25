c707eb7e06749bd0907b2a97624a842d20484fb4
Adeir do Bom Fim Junior <4adeirj@gmail.com>
2022-05-25 02:06:49 -0400

    preconnect tags removed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,8 +33,6 @@ class MyDocument extends Document {
         <Html lang="en">
             <Head>
               <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-              <link rel="preconnect" href="https://fonts.googleapis.com" />
-              <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
               <link href="https://fonts.googleapis.com/css2?family=Parisienne&display=swap" rel="stylesheet" />
             </Head>
             <body>
