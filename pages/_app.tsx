18d9670b21b1e9143779e83eab2c5d420ba370f5
Adeir do Bom Fim Junior <4adeirj@gmail.com>
2022-05-25 19:05:41 -0400

    trying to fix fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { Layout } from '../components';
 import GlobalStyle from '../global/styles/globalStyles';
 import { StateContext } from '../global/context/StateContext';
 import { useRouter } from 'next/router';
-import { ThemeProvider } from 'styled-components';
 import { useStateContext } from '../global/context/StateContext';
 
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,8 +32,9 @@ class MyDocument extends Document {
     return (
         <Html lang="en">
             <Head>
-              <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-              <link href="https://fonts.googleapis.com/css2?family=Parisienne&display=swap" rel="stylesheet" />
+              <link rel="preconnect" href="https://fonts.googleapis.com" />
+              <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+              <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Parisienne&display=swap" rel="stylesheet" />
             </Head>
             <body>
               <Main />
