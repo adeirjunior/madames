d5aed0e17078dc46cfb7b00a720fafd398fb9de2
Adeir do Bom Fim Junior <4adeirj@gmail.com>
2022-05-27 15:24:48 -0400

    logo added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,6 +32,7 @@ class MyDocument extends Document {
     return (
         <Html lang="en">
             <Head>
+              <link rel="icon" href="assets/icons/m.png" type="image/icon type" />
               <link rel="preconnect" href="https://fonts.googleapis.com" />
               <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
               <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Parisienne&display=swap" rel="stylesheet" />
