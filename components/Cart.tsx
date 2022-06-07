f3af35c72ca6e741c6bfa7056cbed0fe9521699b
Adeir do Bom Fim Junior <4adeirj@gmail.com>
2022-06-06 21:43:11 -0400

    styled background cart

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -91,7 +91,6 @@ const Background = styled(Div)`
   width: 100%;
   height: 100vh;
   position: fixed;
-  cursor: pointer;
   display: ${props => props.active ? 'block' : 'none'};
   opacity: ${props => props.active ? 1 : 0};
   top: 0;
